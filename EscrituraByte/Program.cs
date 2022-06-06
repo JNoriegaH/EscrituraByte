88a555a31b77b67cb6b7764d7b52670559c5f0e3
JNoriegaH <jorgeivan0317@gmail.com>
2022-06-06 11:01:32 -0700

    Ejemplo

--- EscrituraByte/Program.cs
+++ EscrituraByte/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace EscrituraByte
 {
     class Program
     {
         static void Main(string[] args)
         {
+            FileStream fs = null;
+            byte[] buffer = new byte[81];
+            int nbytes = 0;
+            int car;
+
+            try
+            {
+                //Crear el flujo del archivo
+                fs = new FileStream("text.txt", FileMode.Create, FileAccess.Write);
+                Console.WriteLine("Escribiendo el texto que desea almacenar en el archivo:");
+                while ((car = (Console.Read())) != '\r' && (nbytes < buffer.Length))
+                {
+                    buffer[nbytes] = (byte)car;
+                    nbytes++;
+                }
+                fs.Write(buffer, 0, nbytes);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(fs !=null)fs.Close();
+            }
         }
     }
 }
